7c7cd073ed7d852fafe5ba6efb7f0c1453931576
Kyler <kylerlepp8@gmail.com>
2022-08-24 12:17:00 -0700

    finished exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,8 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total = Store.sum(:annual_revenue)
+total2 = Store.count()
+puts total / total2
+ai = Store.where('annual_revenue > 1000000').count()
+puts ai
